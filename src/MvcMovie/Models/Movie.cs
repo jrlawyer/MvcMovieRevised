f1591d22149555e50903b5ed776f4c284507a30e
Jennifer <jrlawyer@gmail.com>
2016-07-26 16:59:18 -0400

    I added the filter function.

--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -7,6 +7,9 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
         public string Genre { get; set; }
