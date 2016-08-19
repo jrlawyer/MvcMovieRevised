e6bf98d90a93c5735a3e8bd2ac73a96521dd4b4b
Jennifer <jrlawyer@gmail.com>
2016-08-19 11:11:32 -0400

    I updated the image to the site.css file to change the image for deployment.

---
+++ src/MvcMovie/Migrations/20160819131829_Stringlength.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Stringlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Opinion",
+                table: "Movie",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Opinion",
+                table: "Movie",
+                nullable: true);
+        }
+    }
+}
